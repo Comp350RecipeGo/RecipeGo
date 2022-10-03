04135b2201ef1995a26654cedc0b8a837962b822
tildahh <sara.orona761@myci.csuci.edu>
2022-10-02 23:14:35 -0700

    Initial Commit

---
+++ RecipeGo!/RecipeGo_App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeGo_App.swift
+//  RecipeGo!
+//
+//  Created by Matilda on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeGo_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
